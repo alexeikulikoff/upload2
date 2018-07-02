f7e198c6660a63d2248f0bcdaccb9ef4fa3d9129
admin3 <admin3@localhost>
2018-07-02 10:12:08 +0300

    make upload page

---
+++ com.mibs.upload2.mars/build/resources/main/static/js/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+	
+	console.log("hello1");
+	
+	 $("#btnUpload").click( function(){
+		 
+		 console.log("opload clicked");
+	 });
+	
+});
--- com.mibs.upload2.mars/src/main/java/com/mibs/upload2/mars/controllers/UploadController.java
+++ com.mibs.upload2.mars/src/main/java/com/mibs/upload2/mars/controllers/UploadController.java
@@ -1,6 +1,7 @@
 package com.mibs.upload2.mars.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,9 @@ public class UploadController {
 	 @RequestMapping("/test")
 	 public @ResponseBody String test() {
 	        return "test OK";
+	  }
+	 @RequestMapping("/uploader")
+	 public  String index( Model model ) {
+	        return "uploader";
 	  }
 }

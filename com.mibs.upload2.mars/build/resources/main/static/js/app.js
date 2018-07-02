dec29ddc9817ecd906715f9876b6a6108dfc0226
admin3 <admin3@localhost>
2018-07-02 15:23:26 +0300

    add attachments array

--- com.mibs.upload2.mars/build/resources/main/static/js/app.js
+++ com.mibs.upload2.mars/build/resources/main/static/js/app.js
@@ -1,11 +1,30 @@
-
 $(document).ready(function() {
-	
+
 	console.log("hello1");
-	
-	 $("#btnUpload").click( function(){
-		 
-		 console.log("opload clicked");
-	 });
-	
+
+	$("#btnUpload").click(function() {
+
+		console.log("opload clicked");
+		var data = {
+			fname : "Jhon",
+			lname : "Smith",
+			attachements : ["apple", "banano"]
+		};
+		$.ajax({
+			type : "POST",
+			url : "testUpload",
+			data : JSON.stringify(data),
+			contentType : 'application/json',
+			dataType : "html",
+			//headers : headers,
+			success : function(e) {
+
+			   console.log( e );
+			},
+			error : function(e) {
+			  console.log( e );
+			}
+		});
+	});
+
 });
--- com.mibs.upload2.mars/src/main/java/com/mibs/upload2/mars/controllers/UploadController.java
+++ com.mibs.upload2.mars/src/main/java/com/mibs/upload2/mars/controllers/UploadController.java
@@ -2,15 +2,19 @@ package com.mibs.upload2.mars.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mibs.upload2.mars.utils.DataContainer;
+
 @RestController
 public class UploadController {
 
-	 @RequestMapping("/test")
-	 public @ResponseBody String test() {
+	 @RequestMapping("/testUpload")
+	 public @ResponseBody String testUpload( @RequestBody DataContainer dc) {
+		    System.out.println( dc );
 	        return "test OK";
 	  }
 	 @RequestMapping("/uploader")
